53327bc67a7d5a67cc71d53ae9b3ea54101f8b9f
Sibangi15 <sibangichakraborty2005@gmail.com>
2026-03-25 16:01:53 +0530

    Title Updated

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'BLUES',
       debugShowCheckedModeBanner: false,
       home: AuthGate(),
       theme: Provider.of<ThemeProvider>(context).themeData,
